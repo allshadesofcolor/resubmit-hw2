a1d8335229b75eff6caeb758b7b7323e3c4fe95b
allshadesofcolor <163556067+allshadesofcolor@users.noreply.github.com>
2024-05-28 18:39:00 -0500

    first commit

---
+++ hw_assignments2.py
@@ -0,0 +1,51 @@
+# #Lesson 2    
+
+# #Task 1 Code correction
+# num = int(input("Enter a number: "))
+# if num % 2:
+#     print("The number is negative.")
+# elif num == 0:
+#         print("The number is zero")
+# else: 
+#     print("The number is")
+# print(num)
+
+
+
+# #Lesson 2 Task 1 Identify the greatest
+# list1 = [15, 30, 45]
+# list1.sort()
+# print("The largest number is:", max ("list1"))
+
+
+
+# #Lesson 2 Task 2 Indentify the Smallest
+# list2 = [3, 33, 333]
+# list2.sort()
+# print("The smallest number is:", min("list2"))
+
+# list3 = [55, 55, 55]
+# list3.sort()
+# print("All numbers are equal")
+
+
+# #Lesson 2 Task 3 Equality Check
+
+# num_list = [3, 3, 4]
+# print("The smallest number is", min(num_list))
+# print("The largest number is",max (num_list))
+
+
+# #Lesson 2 Leap year checker Task 1
+
+# def leap_year (year):
+#     if  year % 4 == 0: 
+#         if year % 400:
+#             return "true"
+#         elif year % 100:
+#             return "false"
+#         else:
+#             return "true"
+# #    return "false"
+# # print(leap_year(2023))  
+
